5bb6e5c7a85d248f2efee12c45d8a90835a846b5
Nikolay Khomiakov <nikolay.khomiakov@gmail.com>
2023-08-24 13:47:48 -0400

    function ready

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,49 @@
-import Bowman from './Bowman.js';
-import Daemon from './Daemon.js';
-import Magician from './Magician.js';
-import Swordsman from './Swordsman.js';
-import Undead from './Undead.js';
-import Zombie from './Zombie.js';
+// import Bowman from './Bowman.js';
+// import Daemon from './Daemon.js';
+// import Magician from './Magician.js';
+// import Swordsman from './Swordsman.js';
+// import Undead from './Undead.js';
+// import Zombie from './Zombie.js';
 
-console.log('app worked', Bowman, Daemon, Magician, Swordsman, Undead, Zombie);
+// console.log('app worked', Bowman, Daemon, Magician, Swordsman, Undead, Zombie);
+
+const sortObj = {
+  name: 'мечник', health: 10, level: 2, attack: 80, defence: 40,
+};
+const forSort = ['name', 'level'];
+
+function orderByProps(obj, sort) {
+  const objCopy = {};
+  Object.assign(objCopy, obj);
+  const newArrObj = [];
+
+  for (let i = 0; i < sort.length; i += 1) {
+    for (const propObj in obj) {
+      if (sort[i] === propObj) {
+        const newObj = {};
+        newObj.key = propObj;
+        newObj.value = obj[propObj];
+        newArrObj.push(newObj);
+        delete objCopy[propObj];
+      }
+    }
+  }
+  const sortedList = [];
+  for (const propObj in objCopy) {
+    if (Object.hasOwn(objCopy, propObj)) {
+      sortedList.push(propObj);
+    }
+  }
+  sortedList.sort();
+
+  for (let y = 0; y < sortedList.length; y += 1) {
+    const newObj = {};
+    newObj.key = sortedList[y];
+    newObj.value = obj[sortedList[y]];
+    newArrObj.push(newObj);
+  }
+
+  return newArrObj;
+}
+
+console.log(orderByProps(sortObj, forSort));
